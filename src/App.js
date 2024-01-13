0e38b76101e6fb4553fc065ddcf0879b91e4c9bc
Karolina Śnieżek <karolina.sniezek@xebia.com>
2024-01-14 00:04:21 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import PopUp from "./components/PopUp";
 const App = () => {
   return (
-    <div className="App">
-
+    <div className="app">
+        <Nav/>
+        <Header/>
+        <Feed/>
+        <PopUp/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+const Header = () => {
+    return (
+        <header>
+            <div className="info-container">
+                <div className="user-info-container">
+                    <h1>username</h1>
+                    <p>handle <span className="threads-info">threads.net</span></p>
+                </div>
+            </div>
+            <div className="img-container">
+                <img  src="" alt="profile avatar"/>
+            </div>
+            <p>bio</p>
+            <div className="sub-info-container">
+                <p className="sub-text">X followers •
+                    <a href=""> link</a>
+                </p>
+            </div>
+            <button
+                className="primary"
+                onClick={() => navigator.clipboard.writeText('url')}
+            >
+                Share Profile
+            </button>
+            <div className="button-container">
+                <button className="current">Threads</button>
+                <button>Replies</button>
+            </div>
+        </header>
+    );
+}
+export default Header;
