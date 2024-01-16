b83f811142fd0593ea7c81c670835a2528c7fae2
Karolina Śnieżek <karolina.sniezek@xebia.com>
2024-01-16 23:50:33 +0100

    add app.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,77 @@
+import {useState, useEffect} from "react";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Feed from "./components/Feed";
-import PopUp from "./components/PopUp";
+
 const App = () => {
-  return (
-    <div className="app">
-        <Nav/>
-        <Header/>
-        <Feed/>
-        <PopUp/>
-    </div>
-  );
+    const [ user, setUser ] = useState(null)
+    const [threads, setThreads] = useState([])
+    const [ viewThreadsFeed, setViewThreadsFeed ] = useState(true)
+    const [filteredThreads, setFilteredThreads] = useState([])
+
+    const userId = "6d7e0175-2613-4d29-ac67-215280f0f0df"
+
+    const getUser = async () => {
+        const response = await fetch(`http://localhost:3000/users?user_uuid=${userId}`)
+        const data = await response.json()
+        setUser(data[0])
+        try {
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const getThreads = async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/threads?thread_from=${userId}`)
+            const data = await response.json()
+            setThreads((data))
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const getThreadsFeed = () => {
+        if (viewThreadsFeed) {
+            const standAloneThreads = threads?.filter(thread => thread.reply_to === null)
+            setFilteredThreads(standAloneThreads)
+        }
+        if (!viewThreadsFeed) {
+            const replyThreads = threads?.filter(thread => thread.reply_to !== null)
+            setFilteredThreads(replyThreads)
+        }
+    }
+
+    useEffect(() => {
+        getUser()
+        getThreads()
+    }, [])
+
+    useEffect(() => {
+        getThreadsFeed()
+    },[user, threads, viewThreadsFeed])
+
+    console.log(filteredThreads)
+
+    return (
+        <>
+            { user && <div className="app">
+                <Nav url={user.instagram_url}/>
+                <Header
+                    user={user}
+                    viewThreadsFeed={viewThreadsFeed}
+                    setViewThreadsFeed={setViewThreadsFeed}
+                />
+                <Feed
+                    user={user}
+                    filteredThreads={filteredThreads}
+                />
+                {/*TODO: will be done*/}
+                {/*<PopUp/>*/}
+            </div> }
+        </>
+    );
 }
 
 export default App;
